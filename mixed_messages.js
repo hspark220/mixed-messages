a40a7e3cf9194edd7ca436e484aedadfa83433fb
Heon Soo Park <hspark220@gmail.com>
2021-07-12 13:12:29 -0400

    recreated the project to return three activity suggestions

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,26 +1,31 @@
 //main application for mixed-messages
 
-//beginning of the message (starting simple with 'hello' and 'bye')
-const start = ['hello', 'bye'];
-const middle = ['my', 'crazy', 'random']; //middel of the message
-const end = ['friend', 'dog', 'tree']; //end of the message
+//three different data bases (food, sports, and movies)
+//using these data bases to suggest the days activities
+const foods = ['fried rice', 'french fries', 'sushi', 'fried chicken'];
+const sports = ['play tennis', 'go jogging', 'play soccer','play volleyball']; //middel of the message
+const movies = ['Avengers: End Game', 'Hospital Playlist', 'No Game No Life', 'Iron Man 3']; //end of the message
 
-//takes a array of message datas as a argument
+//takes a array of message datas as a argument and returns a string of the final message
 const messageGenerator = (messageDataBase) => {
     return {
-        _start: messageDataBase[0],
-        _middle: messageDataBase[1],
-        _end: messageDataBase[2],
+        //initializing the databases to the object
+        _foods: messageDataBase[0],
+        _sports: messageDataBase[1],
+        _movies: messageDataBase[2],
+        //generates the message after taking random message from each category
         generateMessage () {
-            const start = this._start[Math.floor(Math.random()*this._start.length)];
-            const middle = this._middle[Math.floor(Math.random()*this._middle.length)];
-            const end = this._end[Math.floor(Math.random()*this._end.length)];
-            const message = `${start}, ${middle} ${end}.`;
-            return message;
+            //taking a random message from each category
+            const food = this._foods[Math.floor(Math.random()*this._foods.length)];
+            const sport = this._sports[Math.floor(Math.random()*this._sports.length)];
+            const movie = this._movies[Math.floor(Math.random()*this._movies.length)];
+            //creating the message
+            const message = `you should eat ${food}.\nyou should ${sport}\nyou should go watch ${movie}.`;
+            return message; //returning the message
         }
     }
 }
 
 //testing to see if the messageGenerator works as intended
-const mg = messageGenerator([start, middle, end]);
+const mg = messageGenerator([foods, sports, movies]);
 console.log(mg.generateMessage());
