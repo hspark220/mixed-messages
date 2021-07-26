ab25cdbaefd9b660210250facf3bcd9e90f038d9
Heon Soo Park <hspark220@gmail.com>
2021-07-26 14:43:48 -0400

    update main.js to collaborate with html and css

--- mixed_messages.js
+++ mixed_messages.js
@@ -3,7 +3,7 @@
 //three different data bases (food, sports, and movies)
 //using these data bases to suggest the days activities
 const foods = ['fried rice', 'french fries', 'sushi', 'fried chicken'];
-const sports = ['play tennis', 'go jogging', 'play soccer','play volleyball']; //middel of the message
+const sports = ['play tennis', 'go jogging', 'play soccer', 'play volleyball']; //middel of the message
 const movies = ['Avengers: End Game', 'Hospital Playlist', 'No Game No Life', 'Iron Man 3']; //end of the message
 
 //takes a array of message datas as a argument and returns a string of the final message
@@ -14,11 +14,11 @@ const messageGenerator = (messageDataBase) => {
         _sports: messageDataBase[1],
         _movies: messageDataBase[2],
         //generates the message after taking random message from each category
-        generateMessage () {
+        generateMessage() {
             //taking a random message from each category
-            const food = this._foods[Math.floor(Math.random()*this._foods.length)];
-            const sport = this._sports[Math.floor(Math.random()*this._sports.length)];
-            const movie = this._movies[Math.floor(Math.random()*this._movies.length)];
+            const food = this._foods[Math.floor(Math.random() * this._foods.length)];
+            const sport = this._sports[Math.floor(Math.random() * this._sports.length)];
+            const movie = this._movies[Math.floor(Math.random() * this._movies.length)];
             //creating the message
             const message = `you should eat ${food}.\nyou should ${sport}\nyou should go watch ${movie}.`;
             return message; //returning the message
@@ -26,6 +26,14 @@ const messageGenerator = (messageDataBase) => {
     }
 }
 
-//testing to see if the messageGenerator works as intended
-const mg = messageGenerator([foods, sports, movies]);
-console.log(mg.generateMessage());
+
+//created for basic hmtl and css page
+let button = document.getElementById('message-button');
+let message = document.getElementById('message');
+
+const showMessage = () => {
+    message.innerHTML = messageGenerator([foods, sports, movies]);
+    button.innerHTML = 'more suggestions';
+}
+
+button.addEventListener('click', showMessage);
